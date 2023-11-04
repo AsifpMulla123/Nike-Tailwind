d8fec94110097efe3f27ea7f9167ebc4f678aa69
Asif <ashupmulla@gmail.com>
2023-11-04 23:07:52 +0530

    Nike Landing Page

---
+++ src/components/Button.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
+  return (
+    <button
+      className={`flex justify-center items-center px-7 py-4 border font-montserrat text-lg leading-none rounded-full gap-2 ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red border-coral-red"
+      }`}
+    >
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewCard = ({ imgURL, rating, feedback }) => {
+  return <div>ReviewCard</div>;
+};
+
+export default ReviewCard;
